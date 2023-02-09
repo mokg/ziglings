c71e8ad1f6452b225d1bb9c919ddfc3902ae7b79
mokg <mokg0502@pm.me>
2023-02-09 21:15:41 +0800

    31 - 40

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -12,9 +12,11 @@ const NumError = error{IllegalNumber};
 pub fn main() void {
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
-
-    try stdout.print("my_num={}\n", .{my_num});
+    if (getNumber()) |my_num| {
+        stdout.print("my_num={}\n", .{my_num}) catch unreachable;
+    } else |_| {
+        unreachable;
+    }
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
